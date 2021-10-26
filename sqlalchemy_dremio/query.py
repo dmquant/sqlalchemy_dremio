c004c0456b4ee8453cd43f294e33589c15809b18
Welly Tambunan <if05041@gmail.com>
2021-10-26 04:18:21 +0000

    adding bool and float32 as data type

--- sqlalchemy_dremio/query.py
+++ sqlalchemy_dremio/query.py
@@ -10,12 +10,14 @@ from pyarrow import flight
 
 _type_map = {
     'boolean': types.BOOLEAN,
+    'bool': types.BOOLEAN,
     'BOOLEAN': types.BOOLEAN,
     'varbinary': types.LargeBinary,
     'VARBINARY': types.LargeBinary,
     'date': types.DATE,
     'DATE': types.DATE,
     'float64': types.FLOAT,
+    'float32': types.FLOAT,
     'decimal': types.DECIMAL,
     'DECIMAL': types.DECIMAL,
     'double': types.FLOAT,
